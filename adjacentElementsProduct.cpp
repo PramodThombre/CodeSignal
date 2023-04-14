395c61e4afb23384cb97e3baad4da51a90b2db97
PramodThombre <61206092+PramodThombre@users.noreply.github.com>
2023-04-14 17:31:29 -0500

    Create adjacentElementsProduct.cpp

---
+++ adjacentElementsProduct.cpp
@@ -0,0 +1,18 @@
+
+*/Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+Example
+For inputArray = [3, 6, -2, -5, 7, 3], the output should be
+solution(inputArray) = 21.
+7 and 3 produce the largest product./*
+
+int solution(vector<int> inputArray) {
+    int max;
+    for(int i=0;i<inputArray.size()-1;i++)
+    {
+        int product=inputArray[i]*inputArray[i+1];
+        if(i==0) max=product;
+        if (max<product)
+            max=product;
+    }
+    return max;
+}
