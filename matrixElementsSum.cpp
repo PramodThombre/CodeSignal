5cbb467c188535f7d73c6013dfeb08779316a836
PramodThombre <61206092+PramodThombre@users.noreply.github.com>
2023-04-23 03:51:02 -0500

    Updated desc

--- matrixElementsSum.cpp
+++ matrixElementsSum.cpp
@@ -1,3 +1,15 @@
+/*Given matrix, a rectangular matrix of integers, where each value represents the cost of the room, 
+your task is to return the total sum of all rooms that are suitable for the CodeBots 
+(ie: add up all the values that don't appear below a 0).
+
+Example
+For
+matrix = [[0, 1, 1, 2], 
+          [0, 5, 0, 0], 
+          [2, 0, 3, 3]]
+the output should be
+solution(matrix) = 9.
+*/
 #include <vector>
 
 int solution(vector<vector<int>> matrix) {
